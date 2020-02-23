e4944dba282e6ba3033245479340e5ca00fa2ad0
Emin91 <emin.proger@gmail.com>
2020-02-24 01:12:32 +0400

    Added

---
+++ app/constants/colorConstans.js
@@ -0,0 +1,16 @@
+export const colors = {
+  RED: '#A02020',
+  GREY: '#424242',
+  WHITE: '#ffffff',
+  BLACK: '#000000',
+  CARROT: '#FE8E2C',
+  LIMEADE: '#81AC00',
+  GREEN: '#00a316',
+  GREY_TWO: '#575757',
+  PELOROUS: '#4C9FC4',
+  CERULEAN: '#039BE5',
+  LOCHMARA: '#0277BD',
+  GREY_OPACITY: '#606060',
+  GREY_OPACITY_TWO: '#b3b3b3',
+  GREY_OPACITY_THREE: '#595959',
+};
